f986eff0b7395998afac98ef859f7ed41c614ca2
Kartik Patel <Paresh@Pareshs-Mac-mini.local>
2016-10-07 17:01:46 +0530

    Created Xcode project
    Created Xcode project

---
+++ KTQuadTree/KTQuadTree/KTQuadTree.h
@@ -0,0 +1,19 @@
+//
+//  KTQuadTree.h
+//  KTQuadTree
+//
+//  Created by Paresh Patel on 10/7/16.
+//  Copyright © 2016 KTPatel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KTQuadTree.
+FOUNDATION_EXPORT double KTQuadTreeVersionNumber;
+
+//! Project version string for KTQuadTree.
+FOUNDATION_EXPORT const unsigned char KTQuadTreeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KTQuadTree/PublicHeader.h>
+
+
